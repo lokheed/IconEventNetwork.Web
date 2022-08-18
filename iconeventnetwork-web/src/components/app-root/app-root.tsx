2cffd270538d60a7f1e97f7389bb4d35607588ff
Ron Miles <rmiles@virtuoso.com>
2022-08-18 13:49:40 -0700

    Modify page routes

--- iconeventnetwork-web/src/components/app-root/app-root.tsx
+++ iconeventnetwork-web/src/components/app-root/app-root.tsx
@@ -14,20 +14,20 @@ export class AppRoot {
         </header>
         <ul class="navigation">
           <li><a href="/">Why Icon?</a></li>
-          <li><a href="/dashboard">Dashboard</a></li>
-          <li><a href="/directory">Directory</a></li>
-          <li><a href="/destinations">Destinations</a></li>
-          <li><a href="/Join">Join the Network</a></li>
+          <li><a href="/dashboard/">Dashboard</a></li>
+          <li><a href="/directory/">Directory</a></li>
+          <li><a href="/destinations/">Destinations</a></li>
+          <li><a href="/Join/">Join the Network</a></li>
         </ul>
         <main>
           <stencil-router>
             <stencil-route-switch scrollTopOffset={0}>
               <stencil-route url="/" component="app-home" exact={true} />
-              <stencil-route url="/dashboard" component="app-dashboard" />
-              <stencil-route url="/directory" component="app-directory" />
-              <stencil-route url="/destinations" component="app-destinations" />
-              <stencil-route url="/join" component="app-join" />
-              <stencil-route url="/profile/:name" component="app-profile" />
+              <stencil-route url="/dashboard/" component="app-dashboard" exact={true} />
+              <stencil-route url="/directory/" component="app-directory" exact={true} />
+              <stencil-route url="/destinations/" component="app-destinations" exact={true} />
+              <stencil-route url="/join/" component="app-join" exact={true} />
+              <stencil-route url="/profile/:name" component="app-profile" exact={true} />
             </stencil-route-switch>
           </stencil-router>
         </main>
