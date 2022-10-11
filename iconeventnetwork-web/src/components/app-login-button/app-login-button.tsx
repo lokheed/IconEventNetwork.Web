a09dbd349935c4f63770f39f9cf686e2295c95bd
Ron Miles <rmiles@IconEventNetwork.com>
2022-10-11 10:39:30 -0400

    Log out both locally and from Cognito

--- iconeventnetwork-web/src/components/app-login-button/app-login-button.tsx
+++ iconeventnetwork-web/src/components/app-login-button/app-login-button.tsx
@@ -6,42 +6,24 @@ import { Component, h } from '@stencil/core';
 })
 
 export class AppLoginButton { 
-  render() {
-    var strapiBaseUrl = 'https://api.iconeventnetwork.com';
-    if (window.location.hostname.toLowerCase() === 'localhost') strapiBaseUrl = 'http://localhost:1337';
-    if (window.location.hostname.toLowerCase().startsWith('qa')) strapiBaseUrl = 'https://qaapi.iconeventnetwork.com';
-    if (window.location.hostname.toLowerCase().startsWith('stg')) strapiBaseUrl = 'https://stgapi.iconeventnetwork.com';
-
-    const loginUrl = strapiBaseUrl + '/api/connect/cognito';
-
-    const logout = (e) => {
-      e.preventDefault();
-      localStorage.removeItem('jwt');
-      localStorage.removeItem('username');
-      window.location.replace('/')
-    };    
-    
+  render() {   
     var isAuthenticated = !!localStorage.getItem('jwt');    
     if (isAuthenticated) {
       return (
         <div>
-          <a onClick={logout}>
+          <a href='/logout'>
             Log Out
           </a> 
         </div>
-
       );  
     } else {
       return (
         <div>
-          <a href={loginUrl}>
+          <a href='/login'>
             Log In
           </a>
         </div>
-
       );  
     }
-
   }
-
 }
--- iconeventnetwork-web/src/components/pages/page-logout/page-logout.tsx
+++ iconeventnetwork-web/src/components/pages/page-logout/page-logout.tsx
@@ -5,9 +5,30 @@ import { Component, h } from '@stencil/core';
 })
 export class PageLogout {;
   render() {
-    localStorage.removeItem('jwt');
-    localStorage.removeItem('username');
-    window.location.replace('/') 
-    return <h1>Logging Out...</h1>
+    var isAuthenticated = !!localStorage.getItem('jwt');
+    if (!isAuthenticated) {
+      window.location.replace('/');
+      return <h1>Logging Out...</h1>
+    } else {
+      localStorage.removeItem('jwt');
+      localStorage.removeItem('username');
+      var cognitoBaseUrl = 'https://iconeventnetwork.auth.us-east-1.amazoncognito.com';
+      var clientId = '6gs898o3k11pu8q57da2p4lkcs';
+      if (window.location.hostname.toLowerCase() === 'localhost') {
+        cognitoBaseUrl = 'http://deviconeventnetwork.auth.us-east-1.amazoncognito.com/';
+        clientId = '794a2cmjve48m7gq4solf4k1v';
+      }
+      if (window.location.hostname.toLowerCase().startsWith('qa')) {
+        cognitoBaseUrl = 'https://qaiconeventnetwork.auth.us-east-1.amazoncognito.com/';
+        clientId = '1k9u2t8al652k79283a6hsqj3';
+      }
+      if (window.location.hostname.toLowerCase().startsWith('stg')) {
+        cognitoBaseUrl = 'https://stgiconeventnetwork.auth.us-east-1.amazoncognito.com/';
+        clientId = '1i6chmckpi833hea22en6r82s2';
+      }
+      var logoutUrl = cognitoBaseUrl + '/logout?client_id=' + clientId + '&logout_uri=' + window.location.href;
+      window.location.replace(logoutUrl);
+      return <h1>Logging Out...</h1>
+    }
   }
 }
