5f50ba33237874ba325ad5fa854ff3f73a013b4b
Ron Miles <rmiles@IconEventNetwork.com>
2023-01-24 10:48:42 -0500

    Formatting tweaks for prelaunch

--- iconeventnetwork-web/src/components/pages/page-prelaunch/page-prelaunch.tsx
+++ iconeventnetwork-web/src/components/pages/page-prelaunch/page-prelaunch.tsx
@@ -12,7 +12,6 @@ export class PagePrelaunch{
                 <div class='accent-block'>
                     <div class='logo-header'>
                         <img src='/assets/icon/icon.png' class='icon' />
-                        <img src='/assets/icon/TIN-Logo-Horizontal-BLK.png' class='logo' />
                     </div>
                     <hr class='purple'/>
                     <p>
@@ -22,7 +21,7 @@ export class PagePrelaunch{
                     <p> 
                         Coming in 2023.
                         <br/>
-                        For more information, please contact us at <a href='mailto:info@theiconnetwork.com'>info@theiconnetwork.com</a> 
+                        For more information, please contact us at <a href="mailto:info@theiconnetwork.com?subject=Inquiry about the Icon Network">info@theiconnetwork.com</a> 
                         <br/>
                         or call +1-212-476-9444.
                     </p> 
